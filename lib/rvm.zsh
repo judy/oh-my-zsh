4b1eeaa5d2b59053251e8300135e27b9e512c953
Clinton Judy <clinton@westarete.com>
2010-09-27 10:09:26 -0400

    Using system ruby doesn't show rvm ruby string.

--- lib/rvm.zsh
+++ lib/rvm.zsh
@@ -1,7 +1,8 @@
-# get the name of the branch we are on
+# get the name of the ruby-version we are on
 function rvm_prompt_info() {
-  ruby_version=$(~/.rvm/bin/rvm-prompt 2> /dev/null) || return
-  echo "($ruby_version)"
+  if [[ -n $(~/.rvm/bin/rvm-prompt s 2> /dev/null) ]]; then
+    return
+  fi
+  ruby_version=$(~/.rvm/bin/rvm-prompt i v 2> /dev/null) || return
+  echo "$ZSH_THEME_RVM_PROMPT_PREFIX$ruby_version$ZSH_THEME_RVM_PROMPT_SUFFIX"
 }
-
-
